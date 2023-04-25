339edd1909acbf94c352dcfb857c5e46614b4c5b
JaklinB <jaklinbasheva@gmail.com>
2023-04-25 11:45:29 +0300

    fixed parent data error in bottom sheet

--- lib/ui/components/input_bottom_sheet.dart
+++ lib/ui/components/input_bottom_sheet.dart
@@ -3,75 +3,71 @@ import 'package:flutter/material.dart';
 import '../../helpers/theme_helper.dart';
 
 void showInputData(BuildContext context, List<TableRow> tableRows) {
-    showModalBottomSheet(
-      isScrollControlled: true,
-      elevation: 1,
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
-      ),
-      builder: (context) {
-        return Container(
-          height: MediaQuery.of(context).size.height - 250,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
-            color: Colors.white,
-          ),
-          child: Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(20.0)),
-                    color: ThemeHelper.backgroundColor,
-                  ),
-                  child: Row(
-                    children: [
-                      const Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16.0),
-                          child: Text(
-                            'Input Data',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () => Navigator.pop(context),
-                        icon: const Icon(
-                          Icons.close,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 10.0),
-                Flexible(
-                  child: SingleChildScrollView(
+  showModalBottomSheet(
+    isScrollControlled: true,
+    elevation: 1,
+    context: context,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+    ),
+    builder: (context) {
+      return Container(
+        height: MediaQuery.of(context).size.height - 250,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+          color: Colors.white,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+                color: ThemeHelper.backgroundColor,
+              ),
+              child: Row(
+                children: [
+                  const Expanded(
                     child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Table(
-                        border: TableBorder.all(
-                          width: 2,
-                          color: ThemeHelper.backgroundColor,
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        'Input Data',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
                         ),
-                        children: tableRows
                       ),
                     ),
                   ),
-                ),
-              ],
+                  IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        );
-      },
-    );
-  }
+            const SizedBox(height: 10.0),
+            Flexible(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Table(
+                      border: TableBorder.all(
+                        width: 2,
+                        color: ThemeHelper.backgroundColor,
+                      ),
+                      children: tableRows),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
